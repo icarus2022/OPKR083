c14489e3127e303bbcfeca82eec0147d717a6760
Vehicle Researcher <>
2021-03-18 22:20:40 +0900

    openpilot v0.8.3

--- selfdrive/ui/latcontrol.h
+++ selfdrive/ui/latcontrol.h
@@ -26,30 +26,9 @@ bool control_button_screenshot(int touch_x, int touch_y) {
   return false;
 }
 
-static void draw_control_button1(UIState *s, int touch_x, int touch_y) {
-  if (s->vipc_client->connected || s->is_OpenpilotViewEnabled) {
-    int btn_w = 140;
-    int btn_h = 140;
-    int btn_x1 = 1920 - btn_w - 195;
-    int btn_y = 1080 - btn_h - 35;
-    int btn_xc1 = btn_x1 + (btn_w/2);
-    int btn_yc = btn_y + (btn_h/2);
-    nvgBeginPath(s->vg);
-    nvgRoundedRect(s->vg, btn_x1, btn_y, btn_w, btn_h, 100);
-    nvgStrokeColor(s->vg, nvgRGBA(255,255,255,80));
-    nvgStrokeWidth(s->vg, 6);
-    nvgStroke(s->vg);
-    nvgFontSize(s->vg, 45);
-    nvgFillColor(s->vg, nvgRGBA(255,255,255,200));
-    nvgText(s->vg,btn_xc1,btn_yc,"NAVI",NULL);
-  }
-}
-
 bool latcontrol( UIState *s, int touch_x, int touch_y ) {
 
   bool touched = false;
-
-  draw_control_button1(s, touch_x, touch_y);
 
   if ((control_button_clicked1(touch_x,touch_y)) && (s->status != STATUS_OFFROAD) && (s->limit_set_speed == 0) && (s->sidebar_collapsed == true)) {
     Params().write_db_value("LimitSetSpeed", "1", 1);
